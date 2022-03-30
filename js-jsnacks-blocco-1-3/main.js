4618b6d672480f9dbdc51752b35a0148e7386a48
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-03-30 11:39:53 +0200

    initial_commit

---
+++ js-jsnacks-blocco-1-3/main.js
@@ -0,0 +1,17 @@
+/*
+il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+*/
+
+//chiedere all'utente di inserire un numero per 10 volte.
+// creare il programma di sommatoria degli inserimemnti.
+
+const user = ("inserisci un numero") 
+//console.log(user) da metterci il prompt.
+
+//creazione di un Array
+
+const Numbers = [1,2,3,4,5,6,7,8,9,10]
+console.log(Numbers)
+
