6be51cc53999d6ee4bcf21fc9662e71893471209
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-03-30 10:55:16 +0200

    excercise_completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,9 @@ Il software stampa il maggiore.
 
 //chiedere all'utente due numeri da inserire con un prompt
 
-// creo primo prompt
+// creo primo prompt.
+
+/*
  const user_1 = parseInt(prompt("inserisci il primo numero"))
  const user_2 = parseInt(prompt("inserisci il secondo numero"))
 
@@ -16,4 +18,27 @@ Il software stampa il maggiore.
      user_1 < user_2
      console.log("è minore")
  }
+
+ */
+
+
+ /*L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+
+// chiedere all'utente due parole in successione con un propmt.
+// stampare la parola più corta e poi la più'
+
+
+const word_one = prompt("inserisci una parola corta");
+const word_two = prompt("inserisci una parola lunga");
+console.log(word_one, word_two);
+
+if(word_one > word_two){
+    console.log("hai inserito la parola lunga")
+}else {
+    word_one < word_two;
+    console.log("hai inserito la parola più corta")
+}
  
