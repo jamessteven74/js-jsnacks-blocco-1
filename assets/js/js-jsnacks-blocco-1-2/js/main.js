051991621f6a87c263d118433efd7c6e6c7f14db
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-03-30 10:59:41 +0200

    change _folder

---
+++ assets/js/js-jsnacks-blocco-1-2/js/main.js
@@ -0,0 +1,10 @@
+const word_one = prompt("inserisci una parola corta");
+const word_two = prompt("inserisci una parola lunga");
+console.log(word_one, word_two);
+
+if(word_one > word_two){
+    console.log("hai inserito la parola lunga")
+}else {
+    word_one < word_two;
+    console.log("hai inserito la parola più corta")
+}
