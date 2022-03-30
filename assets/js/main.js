cb893046f4cc99f7e6fcc35c738950ac46612a38
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-03-30 10:25:24 +0200

    exsercise_completeted

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/*
+SNACK 1 
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+//chiedere all'utente due numeri da inserire con un prompt
+
+// creo primo prompt
+ const user_1 = parseInt(prompt("inserisci il primo numero"))
+ const user_2 = parseInt(prompt("inserisci il secondo numero"))
+ 
